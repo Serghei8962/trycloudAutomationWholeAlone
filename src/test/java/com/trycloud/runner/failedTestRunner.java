369a3835e268759e2e02c1fd37586a105f02f729
Serghei Griu <griu.serghei@gmail.com>
2022-03-29 13:19:04 -0500

    Starting The Project

---
+++ src/test/java/com/trycloud/runner/failedTestRunner.java
@@ -0,0 +1,13 @@
+package com.trycloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue="com/trycloud/runner/failedTestRunner.java"
+)
+public class failedTestRunner {
+}
